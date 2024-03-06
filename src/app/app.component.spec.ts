495dce7797dfdfbd9d3a4683548e6104285d9153
Adam Smith <>
2024-03-06 15:43:56 +0000

    test(stocklist): fix broken test import

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, RouterTestingModule],
       providers: [provideMockStore()],
     }).compileComponents();
   });
